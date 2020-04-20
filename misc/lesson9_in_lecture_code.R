e665ca7e8a5e623640bb010f33c28fc661899c46
nt246 <nt246@cornell.edu>
2020-04-20 09:00:24 -0400

    Updated in-lecture code

--- misc/lesson9_in_lecture_code.R
+++ misc/lesson9_in_lecture_code.R
@@ -21,12 +21,197 @@ lotr_untidy <- bind_rows(fship, ttow, rking)
 
 # Introducing the flights data --------------------------------------------
 
+planes %>% 
+  count(tailnum) %>% 
+  filter(n > 1)
 
+flights
+dim(flights)
+View(flights)
+
+flights %>% 
+  count(tailnum) %>% 
+  filter(n > 1)
 
 
 # Subsetting the flights data ---------------------------------------------
 
+dim(flights)
+
 flights2 <- flights %>% 
   select(year:day, hour, origin, dest, tailnum, carrier)
+
 flights2
+
+airlines
+
+flights2 %>% 
+  left_join(airlines, by = "carrier")
+
+
+weather
+
+flights2 %>% 
+  left_join(weather)
+
+
+planes
+flights2
+
+flights2 %>% 
+  left_join(planes, by = "tailnum")
+
+
+airports
+flights2
+
+flights2 %>% 
+  left_join(airports, c("origin" = "faa"))
+
+
+
+top_dest <- 
+  flights %>% 
+  count(dest, sort = TRUE) %>% 
+  head(10)
+
+flights2 %>% 
+  semi_join(top_dest)
+
+flights2 %>% 
+  anti_join(planes, by = "tailnum") %>% 
+  count(tailnum, sort = TRUE)
+
+
+
+
+
+
+
+# Factors -----------------------------------------------------------------
+
+# Familiarize yourself with the data
+
+gss_cat
+
+gss_cat %>% 
+  count(race)
+
+ggplot(gss_cat, aes(race)) +
+  geom_bar() +
+  scale_x_discrete(drop = FALSE)
+
+ggplot(gss_cat, aes(rincome)) +
+  geom_bar() +
+  coord_flip()
+
+gss_cat %>% 
+  count(partyid, sort = TRUE)
+
+
+# Re-ordering levels
+
+relig_summary <- gss_cat %>% 
+  group_by(relig) %>% 
+  summarize(
+    tvhours = mean(tvhours, na.rm = TRUE)
+  )
+
+ggplot(relig_summary, aes(tvhours, relig)) +
+  geom_point()
+
+ggplot(relig_summary, aes(tvhours, fct_reorder(relig, tvhours))) +
+  geom_point()
+
+
+rincome_summary <- gss_cat %>% 
+  group_by(rincome) %>% 
+  summarize(age = mean(age, na.rm = TRUE))
+
+ggplot(rincome_summary, aes(age, fct_reorder(rincome, age))) +
+  geom_point()
+
+ggplot(rincome_summary, aes(age, fct_relevel(rincome, "Not applicable"))) +
+  geom_point()
+
+
+by_age <- gss_cat %>% 
+  filter(!is.na(age)) %>% 
+  count(age, marital)
+
+ggplot(by_age, aes(age, n, color = marital)) +
+  geom_line()
+
+ggplot(by_age, aes(age, n, color = fct_reorder2(marital, age, n))) +
+  geom_line()
+
+
+ggplot(gss_cat, aes(fct_infreq(marital))) +
+  geom_bar()
+
+
+
+
+
+# Re-naming levels
+
+gss_cat %>%
+  mutate(partyid = fct_recode(partyid,
+                              "Republican, strong"    = "Strong republican",
+                              "Republican, weak"      = "Not str republican",
+                              "Independent, near rep" = "Ind,near rep",
+                              "Independent, near dem" = "Ind,near dem",
+                              "Democrat, weak"        = "Not str democrat",
+                              "Democrat, strong"      = "Strong democrat"
+  )) %>%
+  count(partyid)
+
+
+gss_cat %>%
+  mutate(partyid = fct_recode(partyid,
+                              "Republican, strong"    = "Strong republican",
+                              "Republican, weak"      = "Not str republican",
+                              "Independent, near rep" = "Ind,near rep",
+                              "Independent, near dem" = "Ind,near dem",
+                              "Democrat, weak"        = "Not str democrat",
+                              "Democrat, strong"      = "Strong democrat",
+                              "Other"                 = "No answer",
+                              "Other"                 = "Don't know",
+                              "Other"                 = "Other party"
+  )) %>%
+  count(partyid)
+
+
+gss_cat %>%
+  mutate(partyid = fct_collapse(partyid,
+                                other = c("No answer", "Don't know", "Other party"),
+                                rep = c("Strong republican", "Not str republican"),
+                                ind = c("Ind,near rep", "Independent", "Ind,near dem"),
+                                dem = c("Not str democrat", "Strong democrat")
+  )) %>%
+  count(partyid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
